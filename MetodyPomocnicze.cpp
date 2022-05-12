c92d986ea4fffc7ba5a5abbd937180b81e66eb64
Robert <lajenz@gmail.com>
2022-05-12 23:20:52 +0200

    Divide KsiazkaAdresowa to smaller class

---
+++ MetodyPomocnicze.cpp
@@ -0,0 +1,8 @@
+#include "MetodyPomocnicze.h"
+
+string MetodyPomocnicze::konwerjsaIntNaString(int liczba) {
+    ostringstream ss;
+    ss << liczba;
+    string str = ss.str();
+    return str;
+}
--- Uzytkownik.h
+++ Uzytkownik.h
@@ -1,3 +1,7 @@
+#ifndef UZYTKOWNIK_H
+#define UZYTKOWNIK_H
+
+
 #include <iostream>
 
 using namespace std;
@@ -16,3 +20,5 @@ public:
  string pobierzLogin();
  string pobierzHaslo();
 };
+
+#endif
